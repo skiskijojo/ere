5c0ce4004fa9efd1df1dc14806758ba85f427043
skiskijojo <masjoarthur@gmail.com>
2024-08-08 20:20:47 +0700

    password
    ere

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+password = ""
+
+for i in range(10):
+    password = password + random.choice(characters)
+
+print(password)
